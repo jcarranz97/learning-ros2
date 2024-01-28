1dcd70d4b36592584f6ff5342c64ebd797b5bdb9
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-01-28 11:28:38 -0600

    Include timer to CPP example node

--- src/udemy_tutorials_cpp/src/my_first_node.cpp
+++ src/udemy_tutorials_cpp/src/my_first_node.cpp
@@ -7,8 +7,19 @@ class MyNode: public rclcpp::Node
         {
             // This is the contructor of MyNode
             RCLCPP_INFO(this->get_logger(), "[MyNode] Hello World CPP Node!");
+            timer_ = this->create_wall_timer(
+                std::chrono::seconds(1),
+                std::bind(&MyNode::timerCallback, this));
         }
     private:
+        void timerCallback()
+        {
+            counter_++;
+            RCLCPP_INFO(
+                this->get_logger(), "Timer callback (%d)!!!", counter_);
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        int counter_ = 0; // Create a counter variable and initialize it to 0
 };
 
 
