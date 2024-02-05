c262e45f3003163d8622065812d097367e6283d0
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-02-05 07:19:03 -0600

    Create BatterNode

--- src/udemy_tutorials_py/setup.py
+++ src/udemy_tutorials_py/setup.py
@@ -30,6 +30,7 @@ setup(
             "add_two_ints_client_oop = udemy_tutorials_py.add_two_ints_client_oop:main",
             "hw_status_publisher = udemy_tutorials_py.hw_status_publisher:main",
             "compute_rectangle_area_server = udemy_tutorials_py.compute_rectangle_area_server:main",
+            "battery_node = udemy_tutorials_py.battery_node:main",
         ],
     },
 )
