f073b82181ef9734e6c106f4cfd659e674009844
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-02-05 07:40:47 -0600

    Implement led_panel_node and fix battery_node server to include message

--- src/udemy_tutorials_py/udemy_tutorials_py/battery_node.py
+++ src/udemy_tutorials_py/udemy_tutorials_py/battery_node.py
@@ -2,6 +2,7 @@
 """Battery node used for activity-004 of ros2-for-begginers Udemy course"""
 import sys
 from functools import partial
+from time import sleep
 import rclpy
 from rclpy.node import Node
 from udemy_tutorials_interfaces.srv import SetLedStatus
@@ -15,6 +16,33 @@ class BatteryNode(Node):
         while not self.led_status_client.wait_for_service(timeout_sec=1.0):
             self.logger.warn("'set_led' service is not available ...")
         self.logger.info("client service found!!")
+        self.__baterry_level = 100
+
+        # Set some battery levels
+        while True:
+            self.logger.info("Setting battery level to 100%")
+            self.battery_level = 100
+            sleep(5)
+            self.logger.info("Setting battery level to 50%")
+            self.battery_level = 50
+            sleep(5)
+            self.logger.info("Setting battery level to 10%")
+            self.battery_level = 10
+            sleep(5)
+
+    @property
+    def battery_level(self):
+        """Getters for battery level"""
+        return self.__baterry_level
+
+    @battery_level.setter
+    def battery_level(self, value):
+        """Setters for battery level"""
+        self.__baterry_level = value
+        if self.__baterry_level < 15:
+            self.set_led_status(1, True)
+        else:
+            self.set_led_status(1, False)
 
     @property
     def logger(self):
@@ -23,7 +51,7 @@ class BatteryNode(Node):
 
     def set_led_status(self, led_number, state):
         """Call service to set led status"""
-        request = SetLedStatus.Request(led=led_number, state=state)
+        request = SetLedStatus.Request(led_number=led_number, state=state)
         led_state = "ON" if state else "OFF"
         self.logger.info(f"Setting led {led_number} to '{led_state}'")
         future = self.led_status_client.call_async(request)
@@ -37,6 +65,7 @@ class BatteryNode(Node):
         if not response.success:
             self.logger.error(
                 f"Something went wrong changing the led {request.led_number} status")
+            self.logger.error(response.message)
             return
         self.logger.info("Led status change done!!!")
 
